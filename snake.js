9d5fe4393055db5b8d0ea60eb945a38396593367
Zen <zxc82990@gmail.com>
2016-11-09 20:21:05 +0800

    background & snakehead

--- snake.js
+++ snake.js
@@ -2,14 +2,19 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
 
-    game.load.image('ball','assets/sprites/body1.png');
-    game.load.image('earth', 'assets/sprites/scorched_earth.png');
-    game.load.image('bullet', 'assets/sprites/bullet.png');
+ 	game.load.image('ball','assets/body1.png');
+    game.load.image('earth', 'assets/scorched_earth.png');
+    game.load.image('bullet', 'assets/bullet.png');
 
 }
 
-function create() {
 
+var snakeHead;
+
+function create() {
+	land = game.add.tileSprite(0, 0, 800, 600, 'earth');
+
+	snakeHead = game.add.sprite(400, 300, 'ball');
 }
 
 function update() {
